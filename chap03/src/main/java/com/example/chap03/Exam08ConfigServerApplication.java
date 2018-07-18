4a7c9499c8d748db04d65959e1c1630f30dbd095
Jongmin Kim <expert0226@gmail.com>
2018-07-19 00:57:07 +0900

    finish exam 3-8

--- chap03/src/main/java/com/example/chap03/Exam08ConfigServerApplication.java
+++ chap03/src/main/java/com/example/chap03/Exam08ConfigServerApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+// https://spring.io/guides/gs/centralized-configuration/
 @SpringBootApplication
-//@EnableConfigServer
+@EnableConfigServer
 public class Exam08ConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(Exam08ConfigServerApplication.class);
