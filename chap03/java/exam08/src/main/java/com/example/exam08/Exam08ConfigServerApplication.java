1ce7b0319b7556965538a271a591b6917919d2c0
Jongmin Kim <expert0226@gmail.com>
2018-07-20 19:14:42 +0900

    solve exam 3-8 problem

--- chap03/java/exam08/src/main/java/com/example/exam08/Exam08ConfigServerApplication.java
+++ chap03/java/exam08/src/main/java/com/example/exam08/Exam08ConfigServerApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-// https://spring.io/guides/gs/centralized-configuration/
+// 참고: https://spring.io/guides/gs/centralized-configuration/
+// 사무실에서는 LoopBack 127.0.0.1 / localhost 안 됨. Read IP 접속 요망
+// http://192.168.2.87:8888/configuration-client/master
 @SpringBootApplication
 @EnableConfigServer
 public class Exam08ConfigServerApplication {
